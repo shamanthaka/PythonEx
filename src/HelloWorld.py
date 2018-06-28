1b71110537c34ef87f60253905f68446d27d6680
shamanthaka <shamanthaka.veerareddy@gmail.com>
2018-06-28 08:24:27 +0530

    Initial commit

--- src/HelloWorld.py
+++ src/HelloWorld.py
@@ -1,1 +1,16 @@
-print("hello world")
+
+
+
+
+n=int(input("Enter the number of elements to be inserted: "))
+a=[]
+for i in range(0,n):
+    elem=int(input("Enter element: "))
+    a.append(elem)
+avg=sum(a)/n
+print("Average of elements in the list",round(avg,2))
+
+
+
+
+#You can use triple-quoted strings. When they're not a docstring (first thing in a class/function/module), they are ignored.
---
+++ src/Quo&Rem.py
@@ -0,0 +1,6 @@
+a = int(input("enter the first number : "))
+b = int(input("enter the second number : "))
+quotient =a//b
+remainder =a%b
+print("quotient is "+ input(quotient))
+print("remainder is "+ input(remainder))
